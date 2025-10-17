bc07514c200a16613b75ee6aada284672b5e7aa3
AhmedHamada986 <ahmedhz485@gmail.com>
2025-10-18 00:52:22 +0300

    Add project files.

---
+++ Core/Store.Domain/Entities/Product/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Domain.Entities.Product
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PicturalUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Core/Store.Domain/Entities/Product/ProductType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Domain.Entities.Product
+{
+    public class ProductType
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }    
+    }
+}
